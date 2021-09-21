316295a1eb59002d520a66ef4743cc6cbec3cbca
MariaEduardaSeichas <oliveiramariaeduarda3478@gmail.com>
2021-09-21 02:18:09 +0000

    repositorio iniciado

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view_engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/',(req,res) => {
+  res.render('index.ejs');
+})
+
+app.get('/noticias',(req,res) => {
+  res.render('noticias/index.ejs');
+})
+
+app.get('/noticias/01',(req,res) => {
+  res.render('not/index.ejs');
+})
+
+
+app.listen(3000,() => {
+  console.log('O servidor esta okay')
+})
